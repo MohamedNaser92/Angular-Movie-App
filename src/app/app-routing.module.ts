1b7e8bc5cdcd934109b76eb20df2396c4785fe99
MohamedNaser92 <mohamed.naser.m92@gmail.com>
2023-09-26 19:57:26 +0300

    adding movie services,routing,and interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './movie/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie/movie-details/movie-details.component';
+import { SearchComponent } from './movie/search/search.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: MovieListComponent },
+	{ path: '/:id', component: MovieDetailsComponent },
+	{ path: 'search', component: SearchComponent },
+	{ path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/movie/services/movie.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MovieInterface } from '../movie-interface';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class MovieService {
+	private apiKey = '26de892603494c5470a3c361874c7c6c';
+	private baseUrl = 'https://api.themoviedb.org/3';
+
+	constructor(private http: HttpClient) {}
+
+	getPopularMovies(page: number) {
+		return this.http.get(
+			`${this.baseUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`
+		);
+	}
+
+	getTopRatedMovies() {
+		return this.http.get(
+			`${this.baseUrl}/movie/top_rated?api_key=${this.apiKey}&page=1`
+		);
+	}
+
+	getMovieDetails(movieId: number) {
+		return this.http.get(
+			`${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}`
+		);
+	}
+	getRecommendedMovies(movieId: number) {
+		return this.http.get(
+			`${this.baseUrl}/movie/${movieId}/recommendations?api_key=${this.apiKey}`
+		);
+	}
+	searchMovies(query: string) {
+		return this.http.get(
+			`${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}`
+		);
+	}
+}
+
+// Popular movies:
+// https://api.themoviedb.org/3/movie/popular?api_key=26de892603494c5470a3c361874c7c6c&page=1
+
+// Top Rated Movies:
+// https://api.themoviedb.org/3/movie/top_rated?api_key=26de892603494c5470a3c361874c7c6c&page=1
+
+// Movie Details:
+// https://api.themoviedb.org/3/movie/2?api_key=26de892603494c5470a3c361874c7c6c
+
+// Recommended Movies:
+//https://api.themoviedb.org/3/movie/2/recommendations?api_key=26de892603494c5470a3c361874c7c6c
+
+//Search Movies:
+//https://api.themoviedb.org/3/search/movie?api_key=26de892603494c5470a3c361874c7c6c&query=batman
