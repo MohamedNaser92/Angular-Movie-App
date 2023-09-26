f6e4da534547cc52ef0fbfeb1d8f90f5cbbfe836
MohamedNaser92 <mohamed.naser.m92@gmail.com>
2023-09-26 03:14:47 +0300

    initialize

---
+++ src/app/movie/movie.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WashlistComponent } from './washlist/washlist.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { SearchComponent } from './search/search.component';
+
+
+
+@NgModule({
+  declarations: [
+    WashlistComponent,
+    MovieDetailsComponent,
+    MovieListComponent,
+    SearchComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MovieModule { }
