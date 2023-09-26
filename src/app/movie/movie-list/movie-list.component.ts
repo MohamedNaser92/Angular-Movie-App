a5020d019fadce9b58590e4f18d54bb7676f8674
Ahmed Helmy <42387111+AhmddHelmy@users.noreply.github.com>
2023-09-26 23:16:56 +0300

    Initial home design and movies card design

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MovieInterface } from '../movie-interface';
+import { MovieService } from '../services/movie.service'; 
+
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  styleUrls: ['./movie-list.component.css'],
 })
 export class MovieListComponent {
-
+  trendingMovies:MovieInterface[] = [];
+  posterPrefix:string = 'https://image.tmdb.org/t/p/w500/';
+  constructor (_MovieService: MovieService){
+    _MovieService.getPopularMovies(1).subscribe ((data) => {
+      console.log(data);
+      
+      this.trendingMovies = data.results;
+    });
+  }
 }
