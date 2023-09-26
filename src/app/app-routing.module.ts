3c417201a30a67dd17aafa421a08e256de65a2f6
amrng <amr.ng21@gmail.com>
2023-09-27 02:05:11 +0300

    Design watch-list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MovieListComponent } from './movie/movie-list/movie-list.component';
-import { MovieDetailsComponent } from './movie/movie-details/movie-details.component';
-import { SearchComponent } from './movie/search/search.component';
-import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { MovieListComponent } from "./movie/movie-list/movie-list.component";
+import { MovieDetailsComponent } from "./movie/movie-details/movie-details.component";
+import { SearchComponent } from "./movie/search/search.component";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
+import { WashlistComponent } from "./movie/washlist/washlist.component";
 
 const routes: Routes = [
-	{ path: '', component: MovieListComponent },
-	{ path: 'search', component: SearchComponent },
-	{ path: 'details/:id', component: MovieDetailsComponent },
-	{ path: '**', component: NotFoundComponent },
+  { path: "", component: MovieListComponent },
+  { path: "search", component: SearchComponent },
+  { path: "watch-list", component: WashlistComponent },
+  { path: "details/:id", component: MovieDetailsComponent },
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
