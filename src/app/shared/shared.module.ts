07fa754108151d35effbcadc8991fb9e83625bf8
amrng <amr.ng21@gmail.com>
2023-09-27 00:54:51 +0300

    Make Not Found Page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { RouterLink } from "@angular/router";
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
+  imports: [CommonModule, RouterLink],
 })
-export class SharedModule { }
+export class SharedModule {}
