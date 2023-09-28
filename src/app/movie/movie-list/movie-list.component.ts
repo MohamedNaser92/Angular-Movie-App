38b0fff3d920dce62769a1117f139f6a0db58e33
amrng <amr.ng21@gmail.com>
2023-09-28 15:29:30 +0300

    Edit some UI

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,13 +1,31 @@
-
 import { SearchService } from "../services/search.service";
+import { Component } from "@angular/core";
+import { MovieInterface } from "../movie-interface";
+import { MovieService } from "../services/movie.service";
+import { WatchlistService } from "../services/watchlist.service";
+import { ActivatedRoute, Router, Scroll } from "@angular/router";
 
-
+@Component({
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
+})
 export class MovieListComponent {
   query: string = "";
   searchQuery: string = "";
   filterdMovie: MovieInterface[] = [];
+  trendingMovies: MovieInterface[] = [];
+  currentPage: number = 1;
+  totalPages: number = 500;
+  posterPrefix: string = "https://image.tmdb.org/t/p/w500/";
 
-  constructor(private _SearchService: SearchService) {}
+  constructor(
+    private _SearchService: SearchService,
+    private movieService: MovieService,
+    private watchlist: WatchlistService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   searchMovies(query: string) {
     this._SearchService.getSearchedItems(query);
@@ -17,89 +35,60 @@ export class MovieListComponent {
     this.searchQuery = query;
   }
 
-import { Component } from '@angular/core';
-import { MovieInterface } from '../movie-interface';
-import { MovieService } from '../services/movie.service';
-import { WatchlistService } from '../services/watchlist.service';
-import { ActivatedRoute, Router, Scroll } from '@angular/router';
+  ngOnInit() {
+    // To get pages in home
+    this.route.queryParams.subscribe((params) => {
+      this.currentPage = +params["page"] || 1;
+      this.getPopularMovies();
+    });
+    // To get Movies Stored in WatchList
+    this.watchlist.getWatchlistMovies().subscribe((data) => {});
+  }
 
-@Component({
-	selector: 'app-movie-list',
-	templateUrl: './movie-list.component.html',
-	styleUrls: ['./movie-list.component.css'],
-})
-export class MovieListComponent {
-	trendingMovies: MovieInterface[] = [];
-	currentPage: number = 1;
-	totalPages: number = 500;
+  // Function fetch Popular Movies From API
+  getPopularMovies() {
+    this.movieService.getPopularMovies(this.currentPage).subscribe((data) => {
+      this.trendingMovies = data.results;
+    });
+  }
 
-	posterPrefix: string = 'https://image.tmdb.org/t/p/w500/';
-	constructor(
-		private movieService: MovieService,
-		private watchlist: WatchlistService,
-		private router: Router,
-		private route: ActivatedRoute,
-	) {
-		// _MovieService.getPopularMovies(1).subscribe((data) => {
-		// 	console.log(data);
-		// 	this.trendingMovies = data.results;
-		// });
-	}
+  onPageChange(pageNumber: number) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { page: pageNumber },
+      queryParamsHandling: "merge",
+    });
+    this.getPopularMovies();
+    window.scrollTo(0, 0);
+  }
 
-	ngOnInit() {
-		// To get pages in home
-		this.route.queryParams.subscribe((params) => {
-			this.currentPage = +params['page'] || 1;
-			this.getPopularMovies();
-		});
-		// To get Movies Stored in WatchList
-		this.watchlist.getWatchlistMovies().subscribe((data) => {});
-	}
+  // To Add or Remove Movie From Watchlist
+  toggleWatchlist(movieId: number) {
+    console.log(movieId);
+    this.watchlist.addToWatchlist(movieId).subscribe(() => {});
+  }
+  // To check is the Movie in watchlist
+  isExistInWatchlist(movieId: number): boolean {
+    console.log();
+    return this.watchlist.isExistInWatchlist(movieId);
+  }
 
-	// Function fetch Popular Movies From API
-	getPopularMovies() {
-		this.movieService.getPopularMovies(this.currentPage).subscribe((data) => {
-			this.trendingMovies = data.results;
-		});
-	}
+  // To generate Page Numbers
+  generatePageNumbers(): number[] {
+    const pageNumbers: number[] = [];
 
-	onPageChange(pageNumber: number) {
-		this.router.navigate([], {
-			relativeTo: this.route,
-			queryParams: { page: pageNumber },
-			queryParamsHandling: 'merge',
-		});
-		this.getPopularMovies();
-		window.scrollTo(0, 0);
-	}
+    const maxPagesToShow = 5; // You can adjust this value to show a different number of pages
 
-	// To Add or Remove Movie From Watchlist
-	toggleWatchlist(movieId: number) {
-		console.log(movieId);
-		this.watchlist.addToWatchlist(movieId).subscribe(() => {});
-	}
-	// To check is the Movie in watchlist
-	isExistInWatchlist(movieId: number): boolean {
-		console.log();
-		return this.watchlist.isExistInWatchlist(movieId);
-	}
+    const startPage = Math.max(
+      this.currentPage - Math.floor(maxPagesToShow / 2),
+      1
+    );
+    const endPage = Math.min(startPage + maxPagesToShow - 1, this.totalPages);
 
-	// To generate Page Numbers
-	generatePageNumbers(): number[] {
-		const pageNumbers: number[] = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(i);
+    }
 
-		const maxPagesToShow = 5; // You can adjust this value to show a different number of pages
-
-		const startPage = Math.max(
-			this.currentPage - Math.floor(maxPagesToShow / 2),
-			1
-		);
-		const endPage = Math.min(startPage + maxPagesToShow - 1, this.totalPages);
-
-		for (let i = startPage; i <= endPage; i++) {
-			pageNumbers.push(i);
-		}
-
-		return pageNumbers;
-	}
+    return pageNumbers;
+  }
 }
