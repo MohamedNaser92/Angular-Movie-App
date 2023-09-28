f82fb605122b05f2f176432479b4700257e6351f
amrng <amr.ng21@gmail.com>
2023-09-28 05:51:16 +0300

    made the search feature and minimize overview pipe and some edit in UI

--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
-import { MovieInterface } from '../movie-interface';
-import { MovieService } from '../services/movie.service'; 
-
+import { Component } from "@angular/core";
+import { MovieInterface } from "../movie-interface";
+import { MovieService } from "../services/movie.service";
+import { SearchService } from "../services/search.service";
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css'],
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent {
-  trendingMovies:MovieInterface[] = [];
-  posterPrefix:string = 'https://image.tmdb.org/t/p/w500/';
-  constructor (_MovieService: MovieService){
-    _MovieService.getPopularMovies(1).subscribe ((data) => {
-      console.log(data);
-      
+  trendingMovies: MovieInterface[] = [];
+  posterPrefix: string = "https://image.tmdb.org/t/p/w500/";
+
+  query: string = "";
+  searchQuery: string = "";
+  filterdMovie: MovieInterface[] = [];
+
+  constructor(
+    private _MovieService: MovieService,
+    private _SearchService: SearchService
+  ) {
+    this._MovieService.getPopularMovies(1).subscribe((data) => {
       this.trendingMovies = data.results;
     });
+  }
+
+  searchMovies(query: string) {
+    this._SearchService.getSearchedItems(query);
+    this._SearchService.getMovies().subscribe((val) => {
+      this.filterdMovie = val;
+    });
+    this.searchQuery = query;
   }
 }
